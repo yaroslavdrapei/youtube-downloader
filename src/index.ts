d43e482bb29f8dbe17210be72460721b4101fd14
yaroslavdrapei <yarikdrapey@gmail.com>
2024-09-12 00:06:32 +0300

    chore: add a plan of development to index.ts

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,24 @@ bot.onText(/^\d{1,2}$/, async (msg) => {
 
   bot.downloadByInfo(chatId, index);
 });
+
+/*
+  TODO: 0) Remove all trash branches locally and in the remote
+
+  TODO: 1.1) Create a branch fix/merge-crash
+  TODO: 1.2) Get back to file system instead of streams in order to avoid merge crash issue
+  TODO: 1.3) Merge to refactor/typescript-integration
+
+  TODO: 2.1) Checkout to deploy/docker-compose
+  TODO: 2.2) Dockerize the bot
+  TODO: 2.3) Create a docker compose file
+  TODO: 2.4) Merge with branch refactor/typescript-integration
+  TODO: 2.5) Deploy the bot (optional)
+
+  TODO: 3.1) Create a branch refactor/solid-principles
+  TODO: 3.2) Rewrite the whole project logic 
+  TODO: 3.3) Merge to refactor/typescript-integration
+  TODO: 3.4) Merge to master
+  
+  TODO: 4) Deploy the bot
+*/
