18a20cec511b598fafc20bdf97757fbd2d6513e6
yaroslavdrapei <yarikdrapey@gmail.com>
2024-10-19 13:14:33 +0300

    chore: move to dev bot

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { CommandTexts } from "./modules/CommandTexts";
 
 dotenv.config();
 
-const token = process.env.BOT_TOKEN_PROD;
+const token = process.env.BOT_TOKEN_DEV;
 const port = process.env.PORT || 8081;
 
 const bot = new MyBot(token as string, { polling: true, baseApiUrl: `http://telegram-server:${port}` });
@@ -35,24 +35,3 @@ bot.onText(/^\d{1,2}$/, async (msg) => {
 
   bot.download(chatId, index);
 });
-
-/*
-  TODO: 0) Remove all trash branches locally and in the remote
-
-  TODO: 1.1) Create a branch fix/merge-crash
-  TODO: 1.2) Get back to file system instead of streams in order to avoid merge crash issue
-  TODO: 1.3) Merge to refactor/typescript-integration
-
-  TODO: 2.1) Checkout to deploy/docker-compose
-  TODO: 2.2) Dockerize the bot
-  TODO: 2.3) Create a docker compose file
-  TODO: 2.4) Merge with branch refactor/typescript-integration
-  TODO: 2.5) Deploy the bot (optional)
-
-  TODO: 3.1) Create a branch refactor/solid-principles
-  TODO: 3.2) Rewrite the whole project logic 
-  TODO: 3.3) Merge to refactor/typescript-integration
-  TODO: 3.4) Merge to master
-  
-  TODO: 4) Deploy the bot
-*/
